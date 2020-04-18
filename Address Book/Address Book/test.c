87f14f9a5f9d3c2ccc347fd95fdc3ea7fe531368
张凯 <2748771839@qq.com>
2020-04-18 23:49:13 +0800

    通讯录小项目

--- Address Book/Address Book/test.c
+++ Address Book/Address Book/test.c
@@ -1,1 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include "Address Book.h"
+int main()
+{
+	printf("================");
+	int input = 0;
+	do
+	{
+		printf("1.添加通讯录信息  2.查找通讯录信息");
+	} while (input);
+	return 0;
+}
